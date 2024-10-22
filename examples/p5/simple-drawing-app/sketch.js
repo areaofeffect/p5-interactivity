088ebe21ef9806d6652ae6010b557414bdfe4615
-9 <brunokruse@gmail.com>
2024-10-22 16:44:23 -0400

    initial commit

---
+++ examples/p5/simple-drawing-app/sketch.js
@@ -0,0 +1,14 @@
+function setup() {
+  createCanvas(200, 200);
+  strokeWeight(10)
+  background(127);
+}
+
+function mouseDragged() {
+  // drag to draw a line from previouse mouse position
+  // to current mouse position
+  //stroke(random(255),random(255),random(255));
+  stroke(0);
+  line(mouseX, mouseY, pmouseX, pmouseY);
+  //ellipse(mouseX, mouseY, 10);
+}
